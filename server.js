70b299d7d5b01dee02146f69aec41dcf2e27af08
Hiddevdp <hiddevanderplaat@live.nl>
2024-05-17 22:01:21 +0200

    update

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.static(path.join(__dirname, ".")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./index.html"));
+  res.render("index");
 });
 
 const PORT = process.env.PORT || 3000;
